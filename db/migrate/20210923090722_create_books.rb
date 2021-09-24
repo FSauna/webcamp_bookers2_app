967da411da2cd421b908427ebc4e0392bd4db830
YSauna <yfuta0630@gmail.com>
2021-09-24 14:21:06 +0000

    [Add] bookers 2

--- db/migrate/20210923090722_create_books.rb
+++ db/migrate/20210923090722_create_books.rb
@@ -2,7 +2,8 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :opinion
+      t.string :body
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,8 @@ ActiveRecord::Schema.define(version: 2021_09_23_090722) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "opinion"
+    t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_09_23_090722) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
